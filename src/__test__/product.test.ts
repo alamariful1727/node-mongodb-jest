3545d168bde265da01477f3a015de488b43ffcd8
Ariful Alam <alamariful1727@gmail.com>
2022-11-21 01:04:39 -0500

    feat: cast error test added

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,8 +1,17 @@
+import supertest from "supertest";
+import createServer from "../server";
+
+const app = createServer();
+
 describe("product", () => {
 	describe("get product route", () => {
-		describe("given the product does not exist", () => {
-			it("should return a 404", () => {
-				expect(true).toBe(true);
+		describe("given the productId cast error failed", () => {
+			it("should return a 500", async () => {
+				const productId = "asd";
+				const { statusCode } = await supertest(app).get(
+					`/api/products/${productId}`,
+				);
+				expect(statusCode).toBe(500);
 			});
 		});
 	});
